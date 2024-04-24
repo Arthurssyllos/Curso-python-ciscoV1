3dfdd89d7740dccd0f3dfbadb76fb87be5f4a8c7
Arthur Syllos <arthurpereirasyllos@hotmail.com>
2024-04-24 13:48:58 -0300

    adicionando pasta

--- python-listas-aula06/exemplo-indiceNegativo-lista04.py
+++
@@ -1,6 +0,0 @@
-
-# Criando uma lista
-numeros = [111, 7, 2, 1]
-
-# Imprimindo o último índice da lista (que no caso dessa pequena lista, será o 1)
-print(numeros[-1])
--- python-listas-aula06/python-exemplos-aula04/exemplo-contador.py
+++
@@ -1,7 +0,0 @@
-
-# Contador de 1 a 5 usando while
-
-contador = 1 
-while contador <= 5:
-    print(contador)
-    contador += 1
